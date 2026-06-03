1b630f481e0c885b5a72012597a918c39c5ec1fe
BrandonC <brandonli2007@hotmail.com>
2026-06-03 14:27:59 -0500

    update fix

--- src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
+++ src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
@@ -45,7 +45,7 @@ export const PlaceOrder = () => {
     const resp = await placeOrder( productsToOrder, address);
     if ( !resp.ok ) {
       setIsPlacingOrder(false);
-      setErrorMessage(resp.message);
+      setErrorMessage(resp.message ?? "Error al colocar la orden");
       return;
     }
 
